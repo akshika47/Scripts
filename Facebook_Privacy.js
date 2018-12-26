5561a2401927df39a761190d3b21821aa734b507
Akshika47 <akshikaakalanka@gmail.com>
2018-12-26 20:57:51 +0000

    Added a script for Facebook in order to easily control your privacy

---
+++ Facebook_Privacy.js
@@ -0,0 +1,8 @@
+
+var listofTypes  = document.getElementsByClassName("_42ft _4jy0 _55pi _5vto _55_p _2agf _4o_4 _401v _p _1zg8 _4jy3 _517h _51sy _59pe");
+for(var x=0; x<listofTypes.length; x++)
+{
+	listofTypes[x].click();
+	document.getElementsByClassName("_54nc _54nu")[3].click();
+}
+
